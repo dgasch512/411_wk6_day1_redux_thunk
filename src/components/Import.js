e9185c0e5bb0c16096f3d3a313064cba69f08bcd
dgasch512 <gaschdav208@gmail.com>
2020-04-20 19:49:07 -0500

    Initial commit before JSX

--- src/components/Import.js
+++ src/components/Import.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import { Button, Table, TableHead, TableBody, TableRow, TableCell, Container } from '@material-ui/core';
 
 const Import = (props) => {
     // fill out this component
--- src/containers/Import.js
+++ src/containers/Import.js
@@ -1,1 +1,11 @@
 // fill out this container
+import { connect } from 'react-redux'
+import Import from '../components/Import'
+
+const mapStateToProps = (state) => {
+    return {
+        cars: state.cars
+    }
+}
+
+export default connect(mapStateToProps)(Import)
